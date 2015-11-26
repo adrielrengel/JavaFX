3bd16c6af3c729a2553d8d2d2314563da5606bbe
Adriel Rengel <adrielrengel@gmail.com>
2015-11-26 19:24:32 -0200

    Work

--- src/br/univel/Main.java
+++ src/br/univel/Main.java
@@ -3,6 +3,7 @@ package br.univel;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.fxml.FXMLLoader;
 
@@ -11,7 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("Principal.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Principal.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
